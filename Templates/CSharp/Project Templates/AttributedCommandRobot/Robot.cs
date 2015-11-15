5104e74fcbcb0f8776a96550b6c02c1a398ecdf0
Jeremy Koritzinsky <jkoritzinsky@live.com>
2015-11-14 22:04:00 -0600

    Added some comments to the template.

--- Templates/CSharp/Project Templates/AttributedCommandRobot/Robot.cs
+++ Templates/CSharp/Project Templates/AttributedCommandRobot/Robot.cs
@@ -9,5 +9,7 @@ namespace $safeprojectname$
 {
 	public class $safeprojectname$ : AttributedRobot
     {
+        // You don't need to add any code here unless AttributedRobot can't do something that you need to do.
+        // By default, you can leave this empty.
     }
 }

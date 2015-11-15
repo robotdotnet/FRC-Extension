b28f6d94f26110a10e2736a1c4707a8e4a122ae0
Jeremy Koritzinsky <jkoritzinsky@live.com>
2015-11-14 22:02:22 -0600

    Created AttributedCommandRobot template.

---
+++ Templates/CSharp/Project Templates/AttributedCommandRobot/Commands/ExampleAutonomousCommand.cs
@@ -0,0 +1,44 @@
+﻿using WPILib;
+using WPILib.Commands;
+using WPILib.Extras.AttributedCommandModel;
+
+
+namespace $safeprojectname$.Commands
+{
+    [RunCommandAtPhaseStart(MatchPhase.Autonomous)]
+    public class ExampleAutonomousCommand : Command
+    {
+        public ExampleAutonomousCommand($safeprojectname$.Subsystems.ExampleSubsystem subsystem)
+        {
+            Requires(subsystem);
+        }
+
+        // Called just before this Command runs the first time
+        protected override void Initialize()
+        {
+
+        }
+
+        // Called repeatedly when this Command is scheduled to run
+        protected override void Execute()
+        {
+        }
+
+        // Make this return true when this Command no longer needs to run execute()
+        protected override bool IsFinished()
+        {
+            return false;
+        }
+
+        // Called once after isFinished returns true
+        protected override void End()
+        {
+        }
+
+        // Called when another command which requires one or more of the same
+        // subsystems is scheduled to run
+        protected override void Interrupted()
+        {
+        }
+}
+}
---
+++ Templates/CSharp/Project Templates/AttributedCommandRobot/Subsystems/ExampleSubsystem.cs
@@ -0,0 +1,18 @@
+﻿using WPILib;
+using WPILib.Commands;
+using WPILib.Extras.AttributedCommandModel;
+
+namespace $safeprojectname$.Subsystems
+{
+    [ExportSubsystem(DefaultCommandType = typeof($safeprojectname$.Commands.ExampleCommand))]
+    public class ExampleSubsystem : Subsystem
+    {
+        // Put methods for controlling this subsystem
+        // here. Call these from Commands.
+
+        protected override void InitDefaultCommand()
+        {
+            // This can be left empty since the default command will be set during initialization automatically
+        }
+    }
+}

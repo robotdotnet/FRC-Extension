ee03bb30a2dc9f8ff067ab2eb36d66982eac10d2
Thad House <thadhouse1@gmail.com>
2015-12-11 18:46:41 -0800

    Adds a MonoGame template
    The ones they provide are not very good. We need to use the NuGet
    packages. So created our own.

---
+++ Templates/CSharp/Project-Templates/WindowsMonoGameSimulator/Program.cs
@@ -0,0 +1,30 @@
+﻿using HAL.Simulator;
+using WPILib;
+
+namespace $safeprojectname$
+{
+    public static class Program
+{
+    public static void Main()
+    {
+        RobotBase.Main(null, typeof($mainrobotnamespace.$mainrobotclass));
+    }
+}
+
+public class Simulator : ISimulator
+{
+    public void Initialize()
+    {
+    }
+
+    public void Start()
+    {
+        SimHooks.WaitForProgramStart();
+        DriverStationGUI.DriverStation.StartDriverStationGui();
+        using (var game = new $safeprojectname$())
+                game.Run();
+    }
+
+    public string Name => "Mono Game Simulator";
+}
+}

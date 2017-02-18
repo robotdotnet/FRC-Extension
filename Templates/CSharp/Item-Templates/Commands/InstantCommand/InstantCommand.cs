0f662881df4a4b507a8bbf48e361b7b16dd59784
Matt Soucy <msoucy@csh.rit.edu>
2017-02-18 13:52:57 -0500

    Add InstantCommand template

---
+++ Templates/CSharp/Item-Templates/Commands/InstantCommand/InstantCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using WPILib;
+using WPILib.Commands;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : InstantCommand
+    {
+         public $safeitemname$() {
+            // Use Requires() here to declare subsystem dependencies
+            // eg. Requires(chassis);
+        }
+
+        // Called just before this Command runs the first time
+        protected override void Initialize() {
+        }
+    }
+}
